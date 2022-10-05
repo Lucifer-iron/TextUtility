1403b963805f4c41f76716d408756f2659e0e9db
Lucifer-iron <amithubrajpal@csaist.ac.in>
2022-10-06 01:06:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
+import About from './components/About';
 import { Routes, Route } from "react-router-dom";
 
 
@@ -57,7 +58,7 @@ function App() {
 
         <Routes>
         <Route path="/" element={< TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>} />
-        <Route path="about" element={<About mode={mode} />} />
+         <Route path="about" element={<About mode={mode} />}/>
       </Routes>
         
       </div>
